1671d7322b1b5c259a4719d326328c2feafcbc52
Chris Stewart <c.stewart5545@gmail.com>
2021-07-11 18:19:27 -0500

    Check!

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word,times) {
+    let output= "";
+    for(let i = 0; i < times;i++) {
+        output += word;
+    }
+    if(times < 0){output = 'ERROR'}
+    return output;
 };
 
 module.exports = repeatString;
