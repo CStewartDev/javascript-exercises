9e118b4a755f999833e367f418df3677736106d1
Chris Stewart <c.stewart5545@gmail.com>
2021-07-11 21:14:25 -0500

    YUS!

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let result ="";
+    result = str.split('').reverse().join('');
+    return result
 };
+
+reverseString("Boop");
 
 module.exports = reverseString;
