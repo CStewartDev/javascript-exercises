aea53b15e08feec5dc8ac1cb5d78a1e7adc56c44
Chris Stewart <c.stewart5545@gmail.com>
2021-07-12 07:10:32 -0500

    Another one bites the dust

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let fah = (temp - 32) / 1.8;
+  return Math.round(fah*10) / 10
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let cel = temp * 1.8 + 32
+  return Math.round(cel*10) / 10
 };
 
 module.exports = {
