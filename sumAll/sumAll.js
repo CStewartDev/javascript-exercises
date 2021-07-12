a9725dcfd3ac671175d63ddf15e315d040ceae20
Chris Stewart <c.stewart5545@gmail.com>
2021-07-11 22:17:57 -0500

    Pass!

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,4 +1,14 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    if(!Number.isInteger(args[0])|| !Number.isInteger(args[1])) return "ERROR";
+    if(args[0] < 0 || args[1] < 0) return "ERROR";
+
+    args.sort((a,b)=>a-b);
+    let result = 0;
+    for(let i = args[0];i<=args[1]; i++){
+        result += i;
+    }
+    return result;
+
 
 };
 
