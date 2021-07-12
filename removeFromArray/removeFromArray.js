53f7e9a0305c1338af08ec9d03746c1620a9d0d6
Chris Stewart <c.stewart5545@gmail.com>
2021-07-11 21:47:53 -0500

    Filter yo filter yo filter

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    let filtered = arr;
+    for(let i = 0; i< args.length;i++){
+        filtered = filtered.filter((val) =>val !== args[i])
+    }
+    return filtered;   
 };
 
 module.exports = removeFromArray;
