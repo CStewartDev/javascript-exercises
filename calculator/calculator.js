905ff4f2ef4730ef84c0c1846569eadd39565e48
Chris Stewart <c.stewart5545@gmail.com>
2021-07-25 16:40:23 -0500

    calc complete

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(...args) {
+  return args.reduce((total,curr)=> total + curr)
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+  return args.reduce((total,curr)=> total - curr)
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+  return [...args].reduce((total,curr)=> {
+    return total + curr
+  },0)
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  return [...args].reduce((total,curr)=> {
+    return total * curr
+  },1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let product = 1;
+	for(let i=1; i<=a;i++){
+    product *= i;
+  }
+  return product
 };
 
 module.exports = {
