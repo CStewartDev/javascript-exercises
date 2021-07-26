519dd13d9486a76694b86f592732101cb0e59649
Chris Stewart <c.stewart5545@gmail.com>
2021-07-25 19:51:53 -0500

    Another bites the dust

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let noPunc = str.toUpperCase().replace(/\W/g,"")
+    let reverse = noPunc.split('').reverse().join('')
+    return noPunc === reverse
 };
 
 module.exports = palindromes;
